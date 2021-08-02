6bfbdb29f670f4fb22a8df854569c220aadbbc93
Logan Ramos <lramos15@gmail.com>
2021-08-02 12:53:20 -0400

    Bundle App insights module so we can hand patch it

--- .esbuild.config.js
+++ .esbuild.config.js
@@ -2,46 +2,46 @@ const esbuild = require('esbuild');
 
 // Build node packages and their minifed versions
 esbuild.build({
-  entryPoints: ['src/node/telemetryReporter.ts'],
+	entryPoints: ['src/node/telemetryReporter.ts'],
 	tsconfig: "./src/node/tsconfig.json",
-  bundle: true,
-	external: ['vscode', 'applicationinsights'],
+	bundle: true,
+	external: ['vscode'],
 	sourcemap: true,
 	platform: 'node',
-  outfile: 'lib/telemetryReporter.node.js',
+	outfile: 'lib/telemetryReporter.node.js',
 }).catch(() => process.exit(1))
 
 esbuild.build({
-  entryPoints: ['src/node/telemetryReporter.ts'],
+	entryPoints: ['src/node/telemetryReporter.ts'],
 	tsconfig: "./src/node/tsconfig.json",
-  bundle: true,
+	bundle: true,
 	sourcemap: false,
-	external: ['vscode', 'applicationinsights'],
+	external: ['vscode'],
 	minify: true,
 	platform: 'node',
-  outfile: 'lib/telemetryReporter.node.min.js',
+	outfile: 'lib/telemetryReporter.node.min.js',
 }).catch(() => process.exit(1))
 
 // Build browser packages and their minified versions
 esbuild.build({
-  entryPoints: ['src/browser/telemetryReporter.ts'],
+	entryPoints: ['src/browser/telemetryReporter.ts'],
 	format: "esm",
 	tsconfig: "./src/browser/tsconfig.json",
-  bundle: true,
+	bundle: true,
 	sourcemap: true,
-	external: ['vscode', '@microsoft/applicationinsights-web'],
+	external: ['vscode'],
 	platform: 'browser',
-  outfile: 'lib/telemetryReporter.web.js',
+	outfile: 'lib/telemetryReporter.web.js',
 }).catch(() => process.exit(1))
 
 esbuild.build({
-  entryPoints: ['src/browser/telemetryReporter.ts'],
+	entryPoints: ['src/browser/telemetryReporter.ts'],
 	tsconfig: "./src/browser/tsconfig.json",
 	format: "esm",
-  bundle: true,
+	bundle: true,
 	sourcemap: false,
-		external: ['vscode', '@microsoft/applicationinsights-web'],
+	external: ['vscode'],
 	minify: true,
 	platform: 'browser',
-  outfile: 'lib/telemetryReporter.web.min.js',
+	outfile: 'lib/telemetryReporter.web.min.js',
 }).catch(() => process.exit(1))
