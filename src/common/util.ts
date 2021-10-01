2220a2469cb0df0de8ca73db980d7043f740f1f8
Logan Ramos <lramos15@gmail.com>
2021-10-01 14:40:18 -0400

    Fix getTelemetryLevel

--- src/common/util.ts
+++ src/common/util.ts
@@ -13,14 +13,14 @@ export const enum TelemetryLevel {
 export function getTelemetryLevel(): TelemetryLevel {
 	const TELEMETRY_CONFIG_ID = "telemetry";
 	const TELEMETRY_CONFIG_ENABLED_ID = "enableTelemetry";
-	const TELEMETRY_CONFIG_LEVEL_ID = "telemetryLevel";
+
 	// Could be undefined in old versions of vs code
-	if (vscode.env.isTelemetryEnabled) {
-		return TelemetryLevel.ON;
+	if (vscode.env.isTelemetryEnabled !== undefined) {
+		return vscode.env.isTelemetryEnabled ? TelemetryLevel.ON : TelemetryLevel.OFF;
 	}
+
 	// We use the old and new setting to determine the telemetry level as we must respect both
 	const config = vscode.workspace.getConfiguration(TELEMETRY_CONFIG_ID);
-	const level = config.get<TelemetryLevel>(TELEMETRY_CONFIG_LEVEL_ID);
 	const enabled = config.get<boolean>(TELEMETRY_CONFIG_ENABLED_ID);
-	return level ?  level : enabled ? TelemetryLevel.ON : TelemetryLevel.OFF;
+	return enabled ? TelemetryLevel.ON : TelemetryLevel.OFF;
 }
